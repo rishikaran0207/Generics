61bde2de9fc15024a5c755afc7586f7725ff03d1
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-10 19:47:34 +0530

    Added: Program for Finding Maximum Number:

---
+++ src/main/java/com/javagenerics/Maximum.java
@@ -0,0 +1,24 @@
+package com.javagenerics;
+public class Maximum<E extends Comparable> {
+    E value1,value2,value3;
+
+    public Maximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+    public E testMaximum(){
+        E testMax= testMaximum(value1,value2,value3);
+        return  testMax;
+    }
+
+    public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
+        E max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        System.out.println(max);
+        return max;
+    }
+}
---
+++ src/test/java/com/javagenerics/MaximumTest.java
@@ -0,0 +1,23 @@
+package com.javagenerics;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MaximumTest {
+    @Test
+    public void intMax() {
+        int actualValue = Maximum.testMaximum(9, 4, 1);
+        Assert.assertEquals(9, actualValue);
+    }
+
+    @Test
+    public void floatMax() {
+        float actualValue = Maximum.testMaximum(1.5f,9.1f,4.4f);
+        Assert.assertEquals(9.1f,actualValue,0);
+    }
+
+    @Test
+    public void stringMax() {
+        String actualValue = Maximum.testMaximum("apple","peach","banana");
+        Assert.assertEquals("peach",actualValue);
+    }
+}
