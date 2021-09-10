623c880e934729feecc8116cb2d656a7b7ffc7d4
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-10 23:16:14 +0530

    Added: Extend to max and print

--- src/main/java/com/javagenerics/Maximum.java
+++ src/main/java/com/javagenerics/Maximum.java
@@ -1,30 +1,21 @@
 package com.javagenerics;
 
 public class Maximum<E extends Comparable> {
-    E value1, value2, value3;
-
-    public Maximum(E value1, E value2, E value3) {
-        this.value1 = value1;
-        this.value2 = value2;
-        this.value3 = value3;
-    }
-
-    public E testMaximum() {
-        E testMax = testMaximum(value1, value2, value3);
-        return testMax;
-    }
-
-    public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
-        E max = value1;
-        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
-            max = value2;
-        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
-            max = value3;
-        toPrint(value1, value2, value3, max);
+    public static <E extends Comparable> E testMaximum(E... values) {
+        E max = values[0];
+        for( E element : values){
+            if(element.compareTo(max)>0){
+                max = element;
+            }
+        }
+        toPrint(max,values);
         return max;
     }
 
-    private static <E> void toPrint(E value1, E value2, E value3, E max) {
-        System.out.println("Max of "+value1+" , "+value2+" , "+value3+" is: "+max);
+    private static <E> void toPrint(E max, E... values) {
+        for(E element : values){
+            System.out.print("\t"+element);
+        }
+        System.out.print(" max is "+max);
     }
 }
