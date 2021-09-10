dbb28370b30b909feeb923ca2991614eaf2b412c
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-10 23:09:48 +0530

    Added: Sorting and Extending the parameters

--- src/main/java/com/javagenerics/Maximum.java
+++ src/main/java/com/javagenerics/Maximum.java
@@ -1,14 +1,30 @@
 package com.javagenerics;
 
-public class Maximum {
-    public static String getmaximum(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
-            max = b;
-        else if (c.compareTo(max) > 0)
-            max = c;
-        System.out.println(max);
-        return max;
+public class Maximum<E extends Comparable> {
+    E value1, value2, value3;
 
+    public Maximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public E testMaximum() {
+        E testMax = testMaximum(value1, value2, value3);
+        return testMax;
+    }
+
+    public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
+        E max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        toPrint(value1, value2, value3, max);
+        return max;
+    }
+
+    private static <E> void toPrint(E value1, E value2, E value3, E max) {
+        System.out.println("Max of "+value1+" , "+value2+" , "+value3+" is: "+max);
     }
 }
--- src/test/java/com/javagenerics/MaximumTest.java
+++ src/test/java/com/javagenerics/MaximumTest.java
@@ -1,23 +1,29 @@
 package com.javagenerics;
+
 import org.junit.Test;
 import org.junit.Assert;
 
 public class MaximumTest {
     @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        String finalResult = Maximum.getmaximum("apple", "peach", "banana");
-        Assert.assertEquals("peach", finalResult);
+    public void intMax() {
+        int actualValue = Maximum.testMaximum(9, 4, 1);
+        Assert.assertEquals(9, actualValue);
     }
 
     @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        String finalResult = Maximum.getmaximum("apple", "peach", "banana");
-        Assert.assertEquals("peach", finalResult);
+    public void floatMax() {
+        float actualValue = Maximum.testMaximum(1.5f,9.1f,4.4f);
+        Assert.assertEquals(9.1f,actualValue,0);
     }
 
     @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        String finalResult = Maximum.getmaximum("apple", "peach", "banana");
-        Assert.assertEquals("peach", finalResult);
+    public void stringMax() {
+        String actualValue = Maximum.testMaximum("apple","peach","banana");
+        Assert.assertEquals("peach",actualValue);
+    }
+    @Test
+    public void givenStringValues_WhenGetMaximum_ShouldReturnCorrectValue() {
+        String actualResult = Maximum.testMaximum("Apple", "Peach", "Banana", "Mango","Strawberry");
+        Assert.assertEquals("Strawberry", actualResult);
     }
 }
